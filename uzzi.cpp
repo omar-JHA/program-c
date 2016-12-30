fbe3ebf046e13c0b103d8e48474f3829cc828a99
omarDjesus <omar-k14@hotmail.com>
2016-12-30 14:05:05 -0600

    primeros

---
+++ uzzi.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+#include<math.h>
+using namespace std;
+int main()
+{
+int a;
+int b;
+cout<<"entre valor 1:";
+cin>>a;
+cout<<"entre valor 2:";
+cin>>b;
+cout<<"el producto de los dos numeros es:"<<(a*b)<<endl;
+return 0;
+}
